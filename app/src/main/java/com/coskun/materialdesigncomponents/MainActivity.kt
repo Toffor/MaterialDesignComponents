b3d82c9b630059e59f419f505cf05a6c294e9496
coskun <yalcinkayacoskun@gmail.com>
2018-02-13 22:06:56 +0300

    Navigation Drawer Added.

--- app/src/main/java/com/coskun/materialdesigncomponents/MainActivity.kt
+++ app/src/main/java/com/coskun/materialdesigncomponents/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.coskun.materialdesigncomponents
 
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+fun MainActivity.toast(message: String){
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigation_view.setNavigationItemSelectedListener {
+           onNavigationItemSelected(it.itemId)
+        }
     }
+
+    private fun onNavigationItemSelected(id: Int) : Boolean{
+
+        when (id){
+            R.id.menu_item_1 -> toast("menu item 1")
+            R.id.menu_item_2 -> toast("menu item 2")
+            R.id.menu_item_3 -> toast("menu item 3")
+            R.id.menu_item_4 -> toast("menu item 4")
+            R.id.sub_menu_item_1 -> toast("sub menu item 1")
+            R.id.sub_menu_item_2 -> toast("sub menu item 2")
+            R.id.sub_menu_item_3 -> toast("sub menu item 3")
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
 }
